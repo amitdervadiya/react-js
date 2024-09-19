c80ee6fffdebd55f9e830db3d46e5abc67cffdda
amitdervadiya <amitdervadiya@gmail.com>
2024-09-19 19:27:50 +0530

    reactstrap

---
+++ reactstrap/bootstrap-project/src/App.jsx
@@ -0,0 +1,21 @@
+
+
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar1 from './Compontents/Navbar';
+import Acordian1 from './Compontents/Acordian1';
+import Slider1 from './Compontents/Slider1';
+
+function App() {
+
+
+  return (
+    <>
+      <Navbar1 />
+      <Acordian1 />
+      <Slider1 />
+    </>
+  )
+}
+
+export default App
