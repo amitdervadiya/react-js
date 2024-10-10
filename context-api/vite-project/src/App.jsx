c8884d56351be4505e24ad1f654d053dd16525e8
amitdervadiya <amitdervadiya@gmail.com>
2024-10-10 13:38:38 +0530

    context-api

---
+++ context-api/vite-project/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+import ComOne from './Component/ComOne'
+
+
+function App() {
+
+
+  return (
+    <>
+    <ComOne />
+
+    </>
+  )
+}
+
+export default App
