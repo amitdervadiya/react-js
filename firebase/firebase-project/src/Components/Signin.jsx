4b2cebb81b83c2b64e9557e0c25033feb5a7bfc4
amitdervadiya <amitdervadiya@gmail.com>
2024-11-11 14:06:23 +0530

    firebase

---
+++ firebase/firebase-project/src/Components/Signin.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../firebase';
+import { useNavigate } from 'react-router-dom';
+
+export default function Signin() {
+    const navigate = useNavigate(); 
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const handlesubmit = async (e) => {
+        e.preventDefault();
+        setError(''); 
+        if (email && password) {
+            try {
+                const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+                console.log(userCredential);
+
+              
+                navigate('/signup'); 
+            } catch (err) {
+                console.error('Error signing in:', err.message);
+                setError('Invalid email or password.');
+            }
+        }
+    };
+
+    return (
+        <div style={{ maxWidth: '300px', margin: 'auto', padding: '20px', textAlign: 'center' }}>
+            <form onSubmit={handlesubmit}>
+                <div>
+                    <input
+                        type="email"
+                        placeholder="Email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                        style={{ display: 'block', margin: '10px 0', width: '100%', padding: '8px' }}
+                    />
+                </div>
+                <div>
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                        style={{ display: 'block', margin: '10px 0', width: '100%', padding: '8px' }}
+                    />
+                </div>
+                <button type="submit" style={{ padding: '10px 15px', cursor: 'pointer' }}>
+                    Sign In
+                </button>
+                {error && <p style={{ color: 'red' }}>{error}</p>}
+            </form>
+        </div>
+    );
+}
---
+++ firebase/firebase-project/src/firebase.jsx
@@ -0,0 +1,16 @@
+
+import { initializeApp } from "firebase/app";
+import {getAuth} from 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD5Q1zOqBGVKRbdor1r9axAAxbpIQkxNhw",
+  authDomain: "webstore1234-ae957.firebaseapp.com",
+  projectId: "webstore1234-ae957",
+  storageBucket: "webstore1234-ae957.firebasestorage.app",
+  messagingSenderId: "1007373008716",
+  appId: "1:1007373008716:web:441abbb2c48c13893899e9"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app)
+export { auth };
